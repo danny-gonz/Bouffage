abcc9cc5bff104beed00622c74d01bc24d955e7f
danny-gonz <dpg2012@hotmail.com>
2015-04-09 13:00:04 -0700

    inital commit

---
+++ Bouffage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bouffage.Startup))]
+namespace Bouffage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
